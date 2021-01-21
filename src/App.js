532367ee351861c5a2476f080db76b3475a0bd54
Aove23 <aoveliz.23@gmail.com>
2021-01-21 18:43:09 -0300

    agregando componente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Component from "./components/componente";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hola App!</h2>
+      <Component />
     </div>
   );
 }
