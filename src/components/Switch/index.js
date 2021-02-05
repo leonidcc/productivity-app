79d04a3506f567314e95cd933c16a26b1fb6925f
Leonid <ccleon.id@gmail.com>
2021-02-05 19:06:07 -0300

    modal color

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -2,6 +2,11 @@ import React,  { useState }from "react";
 import styled from "styled-components";
 
 const Combo = styled.header`
+.color{
+    height:20px;
+    width:20px;
+    background:orange;
+}
 .fadeIn{
     animation: example  ease .6s  ;
     z-index:100;
@@ -82,7 +87,7 @@ const Combo = styled.header`
         background:#F07675;
         border:none;
         color:white;
-        padding:5px 15px;
+        padding:7px 25px;
     }
 `;
 
@@ -109,9 +114,55 @@ export default function Switch( {plantillas=["pomodoro","Agustino","pomodoro"]})
 
     <div className={`boxConfig ${modal?"fadeIn":"d-none"}`}>
         <div className="config">
+
+        <div className="d-flex justify-content-between">
         <h3>Settings</h3>
-        <strong>TIME MIUTES</strong>
-        <p>Lorem imsdf sdlfjsdklj flsdkjf orem imsdf sdlfjsdklj flsdkjforem imsdf sdlfjsdklj flsdkjforem imsdf sdlfjsdklj flsdkjf </p>
+        <div type="button" name="button" onClick={()=>{setModal(!modal)}}>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+        </div>
+        </div>
+
+            <hr></hr>
+          <div className="form-group">
+            <label className="d-flex justify-content-between"  >
+            Mombre
+                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                      <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+                </label>
+            <input type="text" className="form-control"  placeholder="Mombre de la configuración"></input>
+           </div>
+
+           <div className="form-row">
+             <div className="col-4">
+             <label  >Ciclo</label>
+               <input type="number" className="form-control" placeholder="Ciclo"></input>
+             </div>
+             <div className="col-4">
+                <label  >Trabajo</label>
+               <input type="number" className="form-control" placeholder="trabajo"></input>
+             </div>
+             <div className="col-4">
+             <label  >Descanso</label>
+               <input type="number" className="form-control" placeholder="descanso"></input>
+             </div>
+           </div>
+           <div className="mt-3 form-group">
+           <label for="exampleColorInput" className="form-label">Color</label>
+           <div className="d-flex">
+           <div className="color form-control  mx-1 form-control-color"> </div>
+           <div className="color form-control bg-success mx-1 form-control-color"> </div>
+           <div className="color form-control bg-primary mx-1 form-control-color"> </div>
+           <div className="color form-control bg-danger mx-1 form-control-color"> </div>
+           <div className="color form-control bg-info mx-1 form-control-color"> </div>
+           </div>
+           </div>
+
+
         <div className="boxBtn">
         <button   type="button" name="button">Apply</button>
         </div>
