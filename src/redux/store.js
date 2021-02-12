63c3eb283cda33bbde878a327faec3f0acef7e8c
Leonid <ccleon.id@gmail.com>
2021-02-12 20:39:28 -0300

    redux selecto reloj y modal

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,11 @@ import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import timerReducer from "./timerData";
+import optionReducer from "./relojTypeData";
 
 const rootReducer = combineReducers({
   test: timerReducer,
+  option: optionReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
